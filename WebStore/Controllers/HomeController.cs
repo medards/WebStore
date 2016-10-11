acf0baa83ec03705cb20b983525d9d62b899501b
Medard <medard.svigelj@hotmail.com>
2016-10-11 18:42:45 +0200

    Added pagination.

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebStore.Models;
+using PagedList;
 
 
 namespace WebStore.Controllers
@@ -15,8 +16,18 @@ namespace WebStore.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        public ActionResult Index(string searchString)
+        public ActionResult Index(string sortOrder, string CurrentFilter,string searchString, int? page)
         {
+            ViewBag.CurrentSort = sortOrder;
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = CurrentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
             var products = from p in db.Products
                            select p;
             if (!String.IsNullOrEmpty(searchString))
@@ -24,7 +35,23 @@ namespace WebStore.Controllers
                 products = products.Where(p => p.Name.Contains(searchString) ||
                 p.Description.Contains(searchString));
             }
-            return View(products.ToList());
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.Name);
+                    break;
+                case "price":
+                    products = products.OrderBy(p => p.Price);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.Name);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(products.ToPagedList(pageNumber,pageSize));
         }
 
         public ActionResult About()
